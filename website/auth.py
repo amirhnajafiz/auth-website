f6fb3687f277f1d75118b0782253792ac8062ca5
9831065 <najafizadeh21@gmail.com>
2021-07-04 18:37:11 +0430

    Auth blueprints added, start templates

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,19 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)  # Creating a blueprint instance
+
+
+# Blueprints and endpoints of the application
+@auth.route('/login')
+def login():  # The login route
+    return "<p>Login</p>"
+
+
+@auth.route('/logout')
+def logout():  # Logging out function
+    return "<p>Logout</p>"
+
+
+@auth.route('/sign-up')
+def sign_up():  # Registering applications
+    return "<p>Sign Up</p>"
--- website/views.py
+++ website/views.py
@@ -2,7 +2,10 @@ from flask import Blueprint
 
 views = Blueprint('views', __name__)  # Creating a blueprint instance
 
+
+
 # Create a view or blueprint
 @views.route('/')
 def home():  # Creating the home end-point
     return "<h1>Test</h1>"
+    
