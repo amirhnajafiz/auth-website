588fd6ac8eebc119eb973c943aee77c9273230ed
9831065 <najafizadeh21@gmail.com>
2021-07-04 18:28:29 +0430

    Start blue prints setup

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,12 @@ def create_app():  # Creating an flask app and setting the secret key
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'iqbnddpoeallwennvcpwee'  # Secret key is for sessions hashing
 
+    # Importing the blueprints
+    from .views import views
+    from .auth import auth
+
+    # Registering the blueprints
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
