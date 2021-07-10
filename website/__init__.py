d6852ff30d1befbbb72c6714629c0151cadcc0e8
9831065 <najafizadeh21@gmail.com>
2021-07-10 13:16:16 +0430

    Setup database

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 
 def create_app():  # Creating an flask app and setting the secret key
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'iqbnddpoeallwennvcpwee'  # Secret key is for sessions hashing
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  # Store the database into a SQLite file
+    db.init_app(app)  # Set the app for database
 
     # Importing the blueprints
     from .views import views
